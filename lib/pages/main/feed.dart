a460353cdd17c736463e19e92d8a6c091abae0be
in-ch <sxin2949@naver.com>
2022-08-13 22:21:26 +0900

    :lipstick: bottom navigator

---
+++ lib/pages/main/feed.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FeedPage extends StatefulWidget {
+  const FeedPage({Key? key}) : super(key: key);
+
+  @override
+  FeedPageState createState() => FeedPageState();
+}
+
+class FeedPageState extends State<FeedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Feed page'));
+  }
+}
