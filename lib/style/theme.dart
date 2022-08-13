98e02e78ab062ea04f9cd083148c33c411fa800b
in-ch <sxin2949@naver.com>
2022-08-13 23:20:43 +0900

    :lipstick: primary color 변경

--- lib/style/theme.dart
+++ lib/style/theme.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 abstract class AppColors {
-  static const secondary = Color(0xFF3B76F6);
+  static const secondary = Color(0xFF6768AA);
   static const accent = Color(0xFFD6755B);
   static const textDark = Color(0xFF53585A);
   static const textLigth = Color(0xFFF5F5F5);
   static const textFaded = Color(0xFF9899A5);
-  static const iconLight = Color(0xFFB1B4C0);
+  static const iconLight = Color(0xFFAEB3DA);
   static const iconDark = Color(0xFFB1B3C1);
   static const textHighlight = secondary;
-  static const cardLight = Color(0xFFF9FAFE);
+  static const cardLight = Color(0xFFF5F5F5);
   static const cardDark = Color(0xFF303334);
 }
 
