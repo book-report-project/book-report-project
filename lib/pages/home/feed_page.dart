8bfc5d9c2da02829f3d38715a1e36cde5ba00a7d
in-ch <sxin2949@naver.com>
2022-08-14 12:16:50 +0900

    :lipstick: 글써달라는 버튼 추가

--- lib/pages/home/feed_page.dart
+++ lib/pages/home/feed_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:book_report_app/style/theme.dart';
+
 class FeedPage extends StatefulWidget {
   const FeedPage({Key? key}) : super(key: key);
 
@@ -10,6 +12,37 @@ class FeedPage extends StatefulWidget {
 class FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Feed page'));
+    return Column(
+      children: [
+        _pleaseWriteBookReport(context),
+      ],
+    );
   }
+}
+
+Widget _pleaseWriteBookReport(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+    child: SizedBox(
+        height: 120,
+        width: MediaQuery.of(context).size.width,
+        child: Align(
+          alignment: Alignment.center,
+          child: SizedBox(
+            height: 40,
+            child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  primary: AppColors.secondary,
+                  backgroundColor: Colors.white,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "🎉  인철 님의 첫 독후감을 작성해주세요.",
+                  style: TextStyle(color: Colors.black, fontSize: 13.0),
+                )),
+          ),
+        )),
+  );
 }
