e6d99b50a074ed977fb36ef04cd05d7aca38682a
in-ch <sxin2949@naver.com>
2022-08-14 17:20:58 +0900

    :lipstick: 글써달라는 버튼에 명언 추가&outlinedButton Theme 적용

--- lib/pages/home/feed_page.dart
+++ lib/pages/home/feed_page.dart
@@ -25,27 +25,58 @@ class FeedPageState extends State<FeedPage> {
 
 Widget _pleaseWriteBookReport(BuildContext context) {
   return Padding(
-    padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-    child: SizedBox(
-        height: 120,
-        width: MediaQuery.of(context).size.width,
-        child: Align(
-          alignment: Alignment.center,
-          child: SizedBox(
-            height: 40,
-            child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  primary: AppColors.secondary,
-                  backgroundColor: Colors.white,
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  "🎉  인철 님의 첫 독후감을 작성해주세요.",
-                  style: TextStyle(color: Colors.black, fontSize: 13.0),
-                )),
-          ),
-        )),
+    padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+    child: Column(
+      children: [
+        SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 40,
+                    child: OutlinedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "🎉  인철 님의 첫 독후감을 작성해주세요.",
+                          style: TextStyle(fontSize: 13.0),
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 8.0, left: 30.0, right: 30.0),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "양서는 처음 읽은 때에는 새 친구를 얻은 것과 같고, 전에 정독했던 책을 다시 읽을 때에는 옛날 친구를 만나는 것과 같다.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: AppColors.textFaded,
+                              fontSize: 11.0,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "- 골드스미스 -",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 11.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )),
+      ],
+    ),
   );
 }
--- lib/style/theme.dart
+++ lib/style/theme.dart
@@ -5,7 +5,7 @@ abstract class AppColors {
   static const secondary = Color(0xFF6768AA);
   static const accent = Color(0xFFD6755B);
   static const textDark = Color(0xFF53585A);
-  static const textLigth = Color(0xFFF5F5F5);
+  static const textLight = Color(0xFFF5F5F5);
   static const textFaded = Color(0xFF9899A5);
   static const iconLight = Color(0xFFAEB3DA);
   static const iconDark = Color(0xFFB1B3C1);
@@ -17,11 +17,15 @@ abstract class AppColors {
 abstract class _LightColors {
   static const background = Colors.white;
   static const card = AppColors.cardLight;
+  static const buttonBackground = AppColors.cardLight;
+  static const buttonTextColor = Colors.black87;
 }
 
 abstract class _DarkColors {
   static const background = Color(0xFF1B1E1F);
   static const card = AppColors.cardDark;
+  static const buttonBackground = AppColors.cardDark;
+  static const buttonTextColor = Colors.white;
 }
 
 /// Reference to the application theme.
@@ -42,6 +46,13 @@ abstract class AppTheme {
       ),
       iconTheme: const IconThemeData(color: AppColors.iconDark),
       colorScheme: ColorScheme.fromSwatch().copyWith(secondary: accentColor),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+            primary: _LightColors.buttonTextColor,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            backgroundColor: _LightColors.buttonBackground),
+      ),
       appBarTheme: const AppBarTheme(
         foregroundColor: AppColors.textDark,
       ));
@@ -50,16 +61,23 @@ abstract class AppTheme {
   static ThemeData dark() => ThemeData(
       visualDensity: visualDensity,
       textTheme:
-          GoogleFonts.interTextTheme().apply(bodyColor: AppColors.textLigth),
+          GoogleFonts.interTextTheme().apply(bodyColor: AppColors.textLight),
       backgroundColor: _DarkColors.background,
       scaffoldBackgroundColor: _DarkColors.background,
       cardColor: _DarkColors.card,
       primaryTextTheme: const TextTheme(
-        headline6: TextStyle(color: AppColors.textLigth),
+        headline6: TextStyle(color: AppColors.textLight),
       ),
       iconTheme: const IconThemeData(color: AppColors.iconLight),
       colorScheme: ColorScheme.fromSwatch().copyWith(secondary: accentColor),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+            primary: _DarkColors.buttonTextColor,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            backgroundColor: _DarkColors.buttonBackground),
+      ),
       appBarTheme: const AppBarTheme(
-        foregroundColor: AppColors.textLigth,
+        foregroundColor: AppColors.textLight,
       ));
 }
