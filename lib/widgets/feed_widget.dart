7d611a42c86ef12d13b277220aefcd7ab961e88c
in-ch <sxin2949@naver.com>
2022-09-10 15:56:48 +0900

    style(feed_widget): 감정 이모지 버튼 추가

--- lib/widgets/feed_widget.dart
+++ lib/widgets/feed_widget.dart
@@ -151,12 +151,103 @@ class FeedWidget extends StatelessWidget {
                                 return const Text('🥰',
                                     style: TextStyle(fontSize: 20.0));
                               },
+                              likeCount: 0,
+                            ),
+                            const SizedBox(width: 7.0),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 5.0),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: const Color(0xffdddddd),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(40)),
+                        child: Row(
+                          children: [
+                            const SizedBox(width: 7.0),
+                            LikeButton(
+                              size: 28,
+                              circleColor: const CircleColor(
+                                  start: Color(0xff00ddff),
+                                  end: Color(0xff0099cc)),
+                              bubblesColor: const BubblesColor(
+                                dotPrimaryColor: Color(0xff33b5e5),
+                                dotSecondaryColor: Color(0xff0099cc),
+                              ),
+                              likeBuilder: (bool isLiked) {
+                                return const Text('🔥',
+                                    style: TextStyle(fontSize: 20.0));
+                              },
+                              likeCount: 0,
+                            ),
+                            const SizedBox(width: 7.0),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 5.0),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: const Color(0xffdddddd),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(40)),
+                        child: Row(
+                          children: [
+                            const SizedBox(width: 7.0),
+                            LikeButton(
+                              size: 28,
+                              circleColor: const CircleColor(
+                                  start: Color(0xff00ddff),
+                                  end: Color(0xff0099cc)),
+                              bubblesColor: const BubblesColor(
+                                dotPrimaryColor: Color(0xff33b5e5),
+                                dotSecondaryColor: Color(0xff0099cc),
+                              ),
+                              likeBuilder: (bool isLiked) {
+                                return const Text('😵',
+                                    style: TextStyle(fontSize: 20.0));
+                              },
                               likeCount: 100,
                             ),
                             const SizedBox(width: 7.0),
                           ],
                         ),
                       ),
+                      const SizedBox(width: 5.0),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: const Color(0xffdddddd),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(40)),
+                        child: Row(
+                          children: [
+                            const SizedBox(width: 7.0),
+                            LikeButton(
+                              size: 28,
+                              circleColor: const CircleColor(
+                                  start: Color(0xff00ddff),
+                                  end: Color(0xff0099cc)),
+                              bubblesColor: const BubblesColor(
+                                dotPrimaryColor: Color(0xff33b5e5),
+                                dotSecondaryColor: Color(0xff0099cc),
+                              ),
+                              likeBuilder: (bool isLiked) {
+                                return const Text('😭',
+                                    style: TextStyle(fontSize: 20.0));
+                              },
+                              likeCount: 100,
+                            ),
+                            const SizedBox(width: 7.0),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 5.0),
                     ],
                   ),
                   Row(
