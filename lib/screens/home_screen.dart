269c1605a6e32dd30343567c3b803daafff6aebb
in-ch <sxin2949@naver.com>
2022-09-22 01:43:33 +0900

    feat(:sparkles:): bottom tab을 tabs 방법으로 수정

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,33 +1,60 @@
+import 'package:book_report_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
-import 'package:book_report_app/pages/pages.dart';
-import 'package:book_report_app/widgets/widgets.dart';
+import 'package:book_report_app/pages/home/feed_page.dart';
+import 'package:book_report_app/pages/home/search_page.dart';
+import 'package:book_report_app/pages/home/write_page.dart';
+import 'package:book_report_app/pages/home/setting_page.dart';
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  HomeState createState() => HomeState();
+}
 
-  final ValueNotifier<int> pageIndex = ValueNotifier(0);
-  final ValueNotifier<String> title = ValueNotifier('독후감 sns 앱');
-
-  final pages = const [FeedPage(), SearchPage(), WritePage(), SettingPage()];
-
-  void _onNavigationItemSelected(index) {
-    pageIndex.value = index;
+class HomeState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: BottomNavigator());
   }
+}
+
+class BottomNavigator extends StatefulWidget {
+  const BottomNavigator({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavigator> createState() => _BottomNavigatorState();
+}
+
+class _BottomNavigatorState extends State<BottomNavigator> {
+  final List<Widget> _widgetOptions = [
+    const FeedPage(),
+    const SearchPage(),
+    const SettingPage(),
+    const WritePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    pageIndex.addListener(() {});
-
-    return Scaffold(
-      appBar: const AppBarWidget(),
-      body: ValueListenableBuilder(
-          valueListenable: pageIndex,
-          builder: (BuildContext context, int value, _) {
-            return pages[value];
-          }),
-      bottomNavigationBar: BottomNavigationBarWidget(
-        onItemSelected: _onNavigationItemSelected,
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 4,
+      child: Scaffold(
+        appBar: const AppBarWidget(),
+        bottomNavigationBar: const TabBar(
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.home_outlined)),
+            Tab(icon: Icon(Icons.search)),
+            Tab(icon: Icon(Icons.movie_outlined)),
+            Tab(icon: Icon(Icons.shopping_bag_outlined)),
+          ],
+          indicatorColor: Colors.red,
+          unselectedLabelColor: Colors.grey,
+          labelColor: Colors.black,
+        ),
+        body: TabBarView(
+          children: _widgetOptions, // 어떤 아이템을 넣어줄 지
+        ),
       ),
     );
   }
